daa6ca64b6f7fa0a68df077747035d19607e78ab
Erableto <alberto_jurado_roldan@outlook.com>
2026-04-30 15:00:42 +0200

    RoundedDropdownMenuTrailingIcon animado

--- app/src/main/java/com/erableto/mywikidexapp/ui/components/RoundedDropdownMenuTrailingIcon.kt
+++ app/src/main/java/com/erableto/mywikidexapp/ui/components/RoundedDropdownMenuTrailingIcon.kt
@@ -1,7 +1,9 @@
 package com.erableto.mywikidexapp.ui.components
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.painterResource
@@ -11,6 +13,18 @@ import com.erableto.mywikidexapp.ui.theme.MyWikiDexAppTheme
 
 @Composable
 fun RoundedDropdownMenuTrailingIcon(expanded: Boolean, modifier: Modifier = Modifier) {
+    val rotation by animateFloatAsState(
+        targetValue = if (expanded) 180f else 0f,
+        label = "RotationAnimation"
+    )
+
+    Icon(
+        painter = painterResource(id = R.drawable.rounded_arrow_drop_down_24),
+        contentDescription = null,
+        modifier = modifier.rotate(rotation)
+    )
+
+    /*
     Icon(
         painter =
             if (expanded) painterResource(id = R.drawable.rounded_arrow_drop_up_24)
@@ -18,6 +32,7 @@ fun RoundedDropdownMenuTrailingIcon(expanded: Boolean, modifier: Modifier = Modi
         contentDescription = null,
         modifier = modifier
     )
+    */
 }
 
 @Preview(showBackground = true)
