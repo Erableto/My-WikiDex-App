09c63c8d5c668f10b03d2788aa93eec7480ceb14
Erableto <alberto_jurado_roldan@outlook.com>
2026-04-30 15:24:26 +0200

    Reordenación variables tipos

--- app/src/main/java/com/erableto/mywikidexapp/util/Utils.kt
+++ app/src/main/java/com/erableto/mywikidexapp/util/Utils.kt
@@ -47,24 +47,24 @@ fun Context.vibrateError() {
 fun getTypeColor(type: String?): Color {
     if (type != null) {
         when (type) {
-            BUG_TYPE -> return colorResource(id = R.color.bug_type)
-            DARK_TYPE -> return colorResource(id = R.color.dark_type)
-            DRAGON_TYPE -> return colorResource(id = R.color.dragon_type)
-            ELECTRIC_TYPE -> return colorResource(id = R.color.electric_type)
-            FAIRY_TYPE -> return colorResource(id = R.color.fairy_type)
-            FIGHTING_TYPE -> return colorResource(id = R.color.fighting_type)
-            FIRE_TYPE -> return colorResource(id = R.color.fire_type)
-            FLYING_TYPE -> return colorResource(id = R.color.flying_type)
-            GHOST_TYPE -> return colorResource(id = R.color.ghost_type)
+            NORMAL_TYPE -> return colorResource(id = R.color.normal_type)
             GRASS_TYPE -> return colorResource(id = R.color.grass_type)
+            FIRE_TYPE -> return colorResource(id = R.color.fire_type)
+            WATER_TYPE -> return colorResource(id = R.color.water_type)
+            ELECTRIC_TYPE -> return colorResource(id = R.color.electric_type)
+            BUG_TYPE -> return colorResource(id = R.color.bug_type)
+            FLYING_TYPE -> return colorResource(id = R.color.flying_type)
+            ROCK_TYPE -> return colorResource(id = R.color.rock_type)
+            POISON_TYPE -> return colorResource(id = R.color.poison_type)
             GROUND_TYPE -> return colorResource(id = R.color.ground_type)
             ICE_TYPE -> return colorResource(id = R.color.ice_type)
-            NORMAL_TYPE -> return colorResource(id = R.color.normal_type)
-            POISON_TYPE -> return colorResource(id = R.color.poison_type)
+            FIGHTING_TYPE -> return colorResource(id = R.color.fighting_type)
             PSYCHIC_TYPE -> return colorResource(id = R.color.psychic_type)
-            ROCK_TYPE -> return colorResource(id = R.color.rock_type)
+            GHOST_TYPE -> return colorResource(id = R.color.ghost_type)
+            DRAGON_TYPE -> return colorResource(id = R.color.dragon_type)
+            DARK_TYPE -> return colorResource(id = R.color.dark_type)
             STEEL_TYPE -> return colorResource(id = R.color.steel_type)
-            WATER_TYPE -> return colorResource(id = R.color.water_type)
+            FAIRY_TYPE -> return colorResource(id = R.color.fairy_type)
             else -> return MaterialTheme.colorScheme.surfaceContainer
         }
     }
