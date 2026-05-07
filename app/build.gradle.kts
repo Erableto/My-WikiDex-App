16d14cdfaeb86676d819c630f1908971d4681c78
Erableto <alberto_jurado_roldan@outlook.com>
2026-05-07 14:51:20 +0200

    v1.0 [1] --> v1.1.0 [110]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.erableto.mywikidexapp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 110
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
