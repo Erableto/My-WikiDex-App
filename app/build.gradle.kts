c7693804d6d0df46db2b16150bbf997c15bae2f4
Erableto <alberto_jurado_roldan@outlook.com>
2026-07-10 17:55:42 +0200

    v1.1.0 [110] --> v1.1.1 [111]
    Fix de velocidad de la animación de la Splash Screen.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.erableto.mywikidexapp"
         minSdk = 24
         targetSdk = 37
-        versionCode = 110
-        versionName = "1.1.0"
+        versionCode = 111
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
